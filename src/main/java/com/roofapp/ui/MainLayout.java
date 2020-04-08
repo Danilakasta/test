f69f822b9b4660bccca4f32b18bd9c8316afbbe8
danil <danilakacta@gmail.com>
2020-04-08 18:43:08 +0300

    add to heroku

--- src/main/java/com/roofapp/ui/MainLayout.java
+++ src/main/java/com/roofapp/ui/MainLayout.java
@@ -37,7 +37,10 @@ import com.roofapp.ui.about.AboutView;
  * The main layout. Contains the navigation menu.
  */
 @Theme(value = Lumo.class)
-@PWA(name = "Rooffactory", shortName = "Roof Factory")
+/*@PWA(name = "Rooffactory",
+        shortName = "RoofFactory",
+        startPath = "login"
+)*/
 @CssImport("./styles/shared-styles.css")
 @CssImport(value = "./styles/menu-buttons.css", themeFor = "vaadin-button")
 public class MainLayout extends AppLayout implements RouterLayout {
